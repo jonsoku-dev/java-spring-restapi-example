178148f5a458af03f2e30d1d2be4fc40cc1da438
jonsoku2 <whdtjr2792@gmail.com>
2020-12-28 16:56:54 +0900

    사용자 등록을 위한 API 구현 - POST HTTP Method

--- src/main/java/com/tamastudy/myrestfulservices/user/UserRestController.java
+++ src/main/java/com/tamastudy/myrestfulservices/user/UserRestController.java
@@ -1,9 +1,7 @@
 package com.tamastudy.myrestfulservices.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,11 @@ public class UserRestController {
             @PathVariable int id
     ) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
     }
 }
