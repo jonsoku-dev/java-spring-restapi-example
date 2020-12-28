78aec414fe294ee15bc75048138e4fc4444b61e9
jonsoku2 <whdtjr2792@gmail.com>
2020-12-28 23:26:52 +0900

    REST API Monitoring 을 위한 Actuator 설정

--- src/main/java/com/tamastudy/myrestfulservices/MyRestfulServicesApplication.java
+++ src/main/java/com/tamastudy/myrestfulservices/MyRestfulServicesApplication.java
@@ -2,23 +2,11 @@ package com.tamastudy.myrestfulservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @SpringBootApplication
 public class MyRestfulServicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyRestfulServicesApplication.class, args);
-    }
-
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-        sessionLocaleResolver.setDefaultLocale(Locale.KOREA);
-        return sessionLocaleResolver;
     }
 }
