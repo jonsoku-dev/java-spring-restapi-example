c868f9a38f66e5f9615c953a6db6008a9d1de7a0
jonsoku2 <whdtjr2792@gmail.com>
2020-12-29 00:12:47 +0900

    JPA 를 이용한 사용자 목록 조회 - GET HTTP Method

--- src/main/java/com/tamastudy/myrestfulservices/user/UserJpaController.java
+++ src/main/java/com/tamastudy/myrestfulservices/user/UserJpaController.java
@@ -1,11 +1,18 @@
 package com.tamastudy.myrestfulservices.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa") // prefix
@@ -17,5 +24,24 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    // http://localhost:8088/jpa/users/1
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+        if (!user.isPresent()) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        // hateoas
+        // "all-users", SERVER_PATH + "/users"
+        // ret
+        EntityModel<User> model = new EntityModel<>(user.get());
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        model.add(linkTo.withRel("all-users"));
+
+        return model;
     }
 }
