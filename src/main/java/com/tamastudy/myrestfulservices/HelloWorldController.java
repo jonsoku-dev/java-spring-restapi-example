6fb44c1462fec4852206dea6baaa9cd509aeb38f
jonsoku2 <whdtjr2792@gmail.com>
2020-12-28 16:20:06 +0900

    Path Variable 사용

--- src/main/java/com/tamastudy/myrestfulservices/HelloWorldController.java
+++ src/main/java/com/tamastudy/myrestfulservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.tamastudy.myrestfulservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +13,11 @@ public class HelloWorldController {
         return "Hello world";
     }
 
-    @GetMapping(path = "/hello-world-bean")
-    public HelloWorldBean helloWorldBean() {
-        return new HelloWorldBean("Hello World");
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(
+            @PathVariable String name
+    ) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 
