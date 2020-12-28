9f93c8aeeb8bdd5a53905e5e190a1da937cefadc
jonsoku2 <whdtjr2792@gmail.com>
2020-12-28 22:28:40 +0900

    Level3 단계의 REST API 구현을 위한 HATEOAS 적용 - 링크시켜서 다른 api의 Uri도 함께 보낸다.

--- src/main/java/com/tamastudy/myrestfulservices/user/UserRestController.java
+++ src/main/java/com/tamastudy/myrestfulservices/user/UserRestController.java
@@ -1,6 +1,8 @@
 package com.tamastudy.myrestfulservices.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -8,6 +10,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,14 +25,23 @@ public class UserRestController {
     }
 
     @GetMapping("/users/{id}")
-    public User user(
+    public EntityModel<User> user(
             @PathVariable int id
     ) {
         User user = service.findOne(id);
         if (user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
-        return user;
+
+        // hateoas
+        // "all-users", SERVER_PATH + "/users"
+        // ret
+        EntityModel<User> model = new EntityModel<>(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).allUsers());
+
+        model.add(linkTo.withRel("all-users"));
+
+        return model;
     }
 
     @PostMapping("/users")
