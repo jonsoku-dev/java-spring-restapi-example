45b0956628d6b21f82657af0dd89ebd40b9bc3c7
jonsoku2 <whdtjr2792@gmail.com>
2020-12-28 16:12:49 +0900

    HelloWorld Bean 추가

---
+++ src/main/java/com/tamastudy/myrestfulservices/HelloWorldBean.java
@@ -0,0 +1,11 @@
+package com.tamastudy.myrestfulservices;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+// Lombok
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+}
--- src/main/java/com/tamastudy/myrestfulservices/HelloWorldController.java
+++ src/main/java/com/tamastudy/myrestfulservices/HelloWorldController.java
@@ -7,8 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     // GET
     // /hello-world (endpoint)
-    @GetMapping("/hello-world")
+    @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello world";
     }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+
 }
