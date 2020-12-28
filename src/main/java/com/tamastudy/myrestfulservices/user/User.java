fc9a683669d8e73a25483b38e88c35fea7d55ba7
jonsoku2 <whdtjr2792@gmail.com>
2020-12-29 00:00:02 +0900

    Spring Data Jpa를 이용한 Entity 설정과 초기 데이터 추가

--- src/main/java/com/tamastudy/myrestfulservices/user/User.java
+++ src/main/java/com/tamastudy/myrestfulservices/user/User.java
@@ -2,8 +2,12 @@ package com.tamastudy.myrestfulservices.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonFilter;
+import io.swagger.annotations.ApiModel;
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -12,9 +16,13 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "All details about the user.")
+@Entity
 //@JsonIgnoreProperties(value = {"password"})
 //@JsonFilter("UserInfo")
 public class User {
+    @GeneratedValue
+    @Id
     private Integer id;
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;
